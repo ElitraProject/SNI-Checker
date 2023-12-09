c17bb9d18950497556b5e1b0680322c032e62ac2
ElitraProject <122404269+ElitraProject@users.noreply.github.com>
2023-12-09 22:29:17 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@ main() {
     sudo apt update
     sudo apt install git pip -y
     git clone https://github.com/ElitraProject/SNI-Checker.git
-    (cd SNI-Checker && pip install -r requirements.txt) 
-    (cd SNI-Checker && python3 main.py)
+    (cd SNI-Checker && pip install -r requirements.txt)
 }
 
 main
